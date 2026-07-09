31fe2cb834518840fb5cdd924876b96c3e44cc68
Kashif Baksh <kashifanisahmedbaksh3@gmail.com>
2026-07-10 02:48:43 +0530

    Upcoming session latest

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { supabase } from "../lib/supabase";
 import { getCurrentUser } from "../lib/auth";
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const user = await getCurrentUser();
